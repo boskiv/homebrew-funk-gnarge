865b25e827b9be1c243d9475958a3b059800e37c
Michael Persson <michael.ake.persson@gmail.com>
2015-06-10 14:07:53 +0200

    Added v0.6

--- tf.rb
+++ tf.rb
@@ -1,16 +1,5 @@
-class Tf < Formula
-  homepage "https://github.com/mickep76/tf"
-  url "https://github.com/mickep76/tf/archive/0.5.tar.gz"
-  sha256 "95f55026f1cd19526387859bd3e69aa5fb44ab518156b465400d386198228d4f"
+  url "https://github.com/mickep76/tf/archive/0.6.tar.gz"
+  sha256 "72ee05d8f00166ec1bdf63b9824573d132782a12e71c8ac4671147b23f7052d3"
 
   depends_on "go" => :build
 
-  def install
-    system "./gobuild.sh"
-    bin.install ".gobuild/bin/tf" => "tf"
-  end
-
-  test do
-    system "#{bin}/tf", "--help"
-  end
-end
