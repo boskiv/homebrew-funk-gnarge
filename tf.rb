bf0cf8376d4ad26490adaf53eaaff1e49f5dfa8d
Michael Persson <michael.ake.persson@gmail.com>
2015-06-10 14:10:56 +0200

    Fix f*ked up rb file

--- tf.rb
+++ tf.rb
@@ -1,5 +1,16 @@
+class Tf < Formula
+  homepage "https://github.com/mickep76/tf"
   url "https://github.com/mickep76/tf/archive/0.6.tar.gz"
   sha256 "72ee05d8f00166ec1bdf63b9824573d132782a12e71c8ac4671147b23f7052d3"
 
   depends_on "go" => :build
 
+  def install
+    system "./gobuild.sh"
+    bin.install ".gobuild/bin/tf" => "tf"
+  end
+
+  test do
+    system "#{bin}/tf", "--help"
+  end
+end
