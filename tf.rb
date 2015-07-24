a39281207009c081ac5b30b749fa95d6e3131d04
Michael Persson <michael.ake.persson@gmail.com>
2015-07-24 16:44:39 +0200

    Bump version

--- tf.rb
+++ tf.rb
@@ -1,7 +1,7 @@
 class Tf < Formula
   homepage "https://github.com/mickep76/tf"
-  url "https://github.com/mickep76/tf/archive/1.6.tar.gz"
-  sha256 "c2f99045e96f97443908e94fad38bd13fa6ab4f87027de7b24afaea212f532da"
+  url "https://github.com/mickep76/tf/archive/1.9.tar.gz"
+  sha256 "667dc1cedd542148972eea14bfe142c0fd80011991292a4288e3c3d2ff767584"
 
   depends_on "go" => :build
 
