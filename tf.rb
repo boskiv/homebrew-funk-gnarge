2b6e02e5388348bde87c500536c9b7ca8ea52219
Michael Persson <michael.ake.persson@gmail.com>
2015-07-24 17:20:43 +0200

    Fix install

--- tf.rb
+++ tf.rb
@@ -1,13 +1,14 @@
 class Tf < Formula
   homepage "https://github.com/mickep76/tf"
-  url "https://github.com/mickep76/tf/archive/1.9-1.tar.gz"
-  sha256 "0859c5ec4196d13bb9106a035da4133ec3341da665ef72ad29167018e1d25abe"
+  url "https://github.com/mickep76/tf/archive/1.9-2.tar.gz"
+  sha256 "73f45a3f2389422c8b17379567b7e91dcc0b361322243ea357e161da781c48fa"
 
   depends_on "go" => :build
 
   def install
-    system "./gobuild.sh"
-    bin.install ".gobuild/bin/tf" => "tf"
+    ENV["GOPATH"] = buildpath
+    system "./build"
+    bin.install "bin/tf" => "tf"
   end
 
   test do
