555f68f2921959f52d480492240240db81038ab8
Michael Persson <michael.ake.persson@gmail.com>
2015-07-28 10:03:22 +0200

    Fix gobuild

--- tf.rb
+++ tf.rb
@@ -1,14 +1,13 @@
 class Tf < Formula
   homepage "https://github.com/mickep76/tf"
-  url "https://github.com/mickep76/tf/archive/2.0.tar.gz"
-  sha256 "3284f02ad08f92a550073b724720eaac3eafc897752f1eebed5406fc56673b07"
+  url "https://github.com/mickep76/tf/archive/2.0-1.tar.gz"
+  sha256 "b114f003c8051576bbc3df3f55bafa3f53a15a992969445ec3583d0cfef9ae00"
 
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    system "./build"
-    bin.install "bin/tf" => "tf"
+    system "./gobuild.sh"
+    bin.install "./gobuild/bin/tf" => "tf"
   end
 
   test do
