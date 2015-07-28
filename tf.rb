bf0f462593bd5d0160433f8e1b5348db32cad325
Michael Persson <michael.ake.persson@gmail.com>
2015-07-28 13:55:26 +0200

    Bumped version

--- tf.rb
+++ tf.rb
@@ -1,13 +1,14 @@
 class Tf < Formula
   homepage "https://github.com/mickep76/tf"
-  url "https://github.com/mickep76/tf/archive/2.0.2.tar.gz"
-  sha256 "ac45d05efb14d47d0c0b7d86192af89fd9818efe5f4cac92cfa706f8869f5aa6"
+  url "https://github.com/mickep76/tf/archive/2.0.3.tar.gz"
+  sha256 "d82242e4e524095b92bda6112bb2af0945a81b7eb1aeb8efc1d6c4d0911e0800"
 
   depends_on "go" => :build
 
   def install
-    system "./gobuild.sh"
-    bin.install "./gobuild/bin/tf" => "tf"
+    ENV["GOPATH"] = buildpath
+    system "./build"
+    bin.install "bin/tf"
   end
 
   test do
