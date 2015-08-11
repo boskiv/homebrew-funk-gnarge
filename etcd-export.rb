d27cbbb46a1dbbad373f5540ce76c3a052ab7c1d
Michael Persson <michael.ake.persson@gmail.com>
2015-08-11 16:53:53 +0200

    Fix class name;

--- etcd-export.rb
+++ etcd-export.rb
@@ -1,4 +1,4 @@
-class Etcd-export < Formula
+class EtcdExport < Formula
   homepage "https://github.com/mickep76/etcd-export"
   url "https://github.com/mickep76/etcd-export/archive/0.2.tar.gz"
   sha256 "195c65effcb9a68b8b598a9c19fff59b7abb6fd1a293db7f1fb10e07ee8a4035"
