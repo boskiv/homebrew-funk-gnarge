29a796b8c42f33a0d7bc779a5a1ab0e950eac7e8
Michael Persson <michael.ake.persson@gmail.com>
2015-09-03 16:14:39 +0200

    Fixed plist

--- peekaboo.rb
+++ peekaboo.rb
@@ -10,8 +10,31 @@ class Peekaboo < Formula
     ENV["GOPATH"] = buildpath
     system "./build"
     bin.install "bin/peekaboo"
-    (var/"peekaboo").mkdir
-    (var/"peekaboo").install "src/github.com/mickep76/peekaboo/static", "src/github.com/mickep76/peekaboo/templates"
+    (prefix/"peekaboo").install "src/github.com/mickep76/peekaboo/static", "src/github.com/mickep76/peekaboo/templates"
+  end
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>KeepAlive</key>
+      <true/>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{bin}/peekaboo</string>
+        <string>--static-dir</string>
+        <string>#{prefix}/peekaboo/static</string>
+        <string>--template-dir</string>
+        <string>#{prefix}/peekaboo/templates</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+    </dict>
+    </plist>
+    EOS
   end
 
   test do
