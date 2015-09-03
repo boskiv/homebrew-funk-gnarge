2f5d66a13492b35d7531417857662ccac32563af
Michael Persson <michael.ake.persson@gmail.com>
2015-09-03 14:10:46 +0200

    Added peekaboo

---
+++ peekaboo.rb
@@ -0,0 +1,17 @@
+class EtcdExport < Formula
+  homepage "https://github.com/mickep76/peekaboo"
+  url "https://github.com/mickep76/peekaboo/archive/0.2.tar.gz"
+  sha256 "026bc507f059c22ba3689f2a21954af9d8ea833bb6fe2b0b083319eef9f82c64"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    system "./build"
+    bin.install "bin/peekaboo"
+  end
+
+  test do
+    system "#{bin}/peekaboo", "--version"
+  end
+end
