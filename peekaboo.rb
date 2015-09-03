32dd67927b5b8132a1cc32f89053b46424d9fa1b
Michael Persson <michael.ake.persson@gmail.com>
2015-09-03 14:14:08 +0200

    Fixed typo

--- peekaboo.rb
+++ peekaboo.rb
@@ -1,4 +1,4 @@
-class EtcdExport < Formula
+class Peekaboo < Formula
   homepage "https://github.com/mickep76/peekaboo"
   url "https://github.com/mickep76/peekaboo/archive/0.2.tar.gz"
   sha256 "026bc507f059c22ba3689f2a21954af9d8ea833bb6fe2b0b083319eef9f82c64"
