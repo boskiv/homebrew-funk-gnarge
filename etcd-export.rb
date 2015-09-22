ff51014b7a26aab13b59dcfbdb0345344b014129
Michael Persson <michael.ake.persson@gmail.com>
2015-09-22 10:41:24 +0200

    Bumped version

--- etcd-export.rb
+++ etcd-export.rb
@@ -1,7 +1,7 @@
 class EtcdExport < Formula
   homepage "https://github.com/mickep76/etcd-export"
-  url "https://github.com/mickep76/etcd-export/archive/1.2.tar.gz"
-  sha256 "366d3804fdb8d4a39e55841c76e5f0b5dbb929bc29fa41de374a427e5a9f2c61"
+  url "https://github.com/mickep76/etcd-export/archive/1.3.tar.gz"
+  sha256 "a6b46a071d9db7098e23b5a30ee7ada3f0ee201062fb06289c522a2772116dee"
 
   depends_on "go" => :build
 
@@ -11,11 +11,13 @@ class EtcdExport < Formula
     bin.install "bin/etcd-export"
     bin.install "bin/etcd-import"
     bin.install "bin/etcd-delete"
+    bin.install "bin/etcd-validate"
   end
 
   test do
     system "#{bin}/etcd-export", "--version"
     system "#{bin}/etcd-import", "--version"
     system "#{bin}/etcd-delete", "--version"
+    system "#{bin}/etcd-validate", "--version"
   end
 end
