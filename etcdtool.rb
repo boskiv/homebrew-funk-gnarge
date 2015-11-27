4ded1c2e535098c46d8353c6d398177e79a313ee
Michael Persson <michael.ake.persson@gmail.com>
2015-11-27 16:52:17 +0100

    Fix

--- etcdtool.rb
+++ etcdtool.rb
@@ -1,4 +1,4 @@
-class EtcdTool < Formula
+class Etcdtool < Formula
   version "2.6"
   sha256 "dfc0c5fee9b20b75bba0f8e051395abf7e3cb61d126dd0df1658a57329543adc"
   homepage "https://github.com/mickep76/etcdtool"
