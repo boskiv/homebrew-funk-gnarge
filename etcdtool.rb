1b0e336cf1ce668a45969b55b93370558481bfb7
Michael Persson <michael.ake.persson@gmail.com>
2015-12-18 12:02:16 +0100

    Bump version

--- etcdtool.rb
+++ etcdtool.rb
@@ -1,6 +1,6 @@
 class Etcdtool < Formula
-  version "2.7"
-  sha256 "7b6f7422fcb60124020cc596bac1baba236027ba2f523e451339b3c3ad66d6aa"
+  version "2.8"
+  sha256 "7b0bd76aec33d6d735b7962e783cfa3da0913a7e6daf2d14ad04c3cd50d6cb50"
   homepage "https://github.com/mickep76/etcdtool"
   url "https://github.com/mickep76/etcdtool/archive/#{version}.tar.gz"
 
