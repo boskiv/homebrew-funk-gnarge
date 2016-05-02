a06aea258cebba2b281528e14fcfdb7dc73d0584
Michael Persson <michael.ake.persson@gmail.com>
2016-05-02 15:29:55 -0500

    fix deps

--- etcdtool.rb
+++ etcdtool.rb
@@ -1,6 +1,6 @@
 class Etcdtool < Formula
-  version "3.3.1"
-  sha256 "7586718edce3e0b737ca6a465c65f7ad4e9439b8c67129786d5b899d3f826bfb"
+  version "3.3.2"
+  sha256 "f9f4aea1e1459e1f187e8f74faec537b97cb2519bdead9f12e4c1bf93a69df6b"
   homepage "https://github.com/mickep76/etcdtool"
   url "https://github.com/mickep76/etcdtool/archive/#{version}.tar.gz"
 
